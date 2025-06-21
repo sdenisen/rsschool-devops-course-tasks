783b4290e84effad7475f8021e79377fdf4440b7
SergeSD <sdenisen@ymail.com>
2025-06-21 23:43:39 +0100

    feat: create vps and networks; passed;

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "availability_zones" {
+  description = "AZs to use"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b"]
+}
