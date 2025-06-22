076688788014593e594a6bfb7d56de107fe084a9
SergeSD <sdenisen@ymail.com>
2025-06-22 11:42:39 +0100

    feat: create/configure internet gateway; passed;

--- vpc.tf
+++ vpc.tf
@@ -47,3 +47,24 @@ resource "aws_subnet" "private-subnet-2" {
     Name = "private-subnet-2"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
